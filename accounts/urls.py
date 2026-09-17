522a14c426da283a04eb8b0535a77add5fe2fb5d
carloscifuentesingsoft1560-cyber <carlos.cifuentes.ing.soft1560@gmail.com>
2026-09-17 07:55:45 -0500

    feat: agrega recuperacion de contrasena

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from accounts import views
@@ -35,5 +36,61 @@ urlpatterns = [
         'salir/',
         views.account_logout,
         name='logout'
+    ),
+
+    # ========================================================
+    # RECUPERACIÓN DE CONTRASEÑA
+    # ========================================================
+
+    path(
+        'recuperar-contrasena/',
+        auth_views.PasswordResetView.as_view(
+            template_name=(
+                'accounts/password_reset_form.html'
+            ),
+            email_template_name=(
+                'accounts/password_reset_email.html'
+            ),
+            subject_template_name=(
+                'accounts/password_reset_subject.txt'
+            ),
+            success_url=(
+                '/cuenta/recuperar-contrasena/enviado/'
+            ),
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'recuperar-contrasena/enviado/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name=(
+                'accounts/password_reset_done.html'
+            )
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'restablecer/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name=(
+                'accounts/password_reset_confirm.html'
+            ),
+            success_url=(
+                '/cuenta/restablecer/completado/'
+            ),
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'restablecer/completado/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name=(
+                'accounts/password_reset_complete.html'
+            )
+        ),
+        name='password_reset_complete'
     ),
 ]
